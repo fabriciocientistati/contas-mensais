01771d9747e810fd278a8f1dba8b538ce7629a37
fabriciocientistati <fabricio.cientistati@gmail.com>
2026-02-07 12:42:38 -0400

    chore(db): adicionar migrations e habilitar migrate

---
+++ back-end/ContasMensais.API/Migrations/20260207162011_AddReceitaMensal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContasMensais.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReceitaMensal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
